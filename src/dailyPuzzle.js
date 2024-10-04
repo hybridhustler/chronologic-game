8310c10fe8b8ebb283c21146bc2c67e358a50a34
hybridhustler <hybridhustlerarmy@gmail.com>
2024-10-03 22:22:12 -0400

    Update puzzle for 10/3

--- src/dailyPuzzle.js
+++ src/dailyPuzzle.js
@@ -1,14 +1,32 @@
-// src/dailyPuzzle.js
-
 const dailyPuzzle = {
   date: '2024-10-03', // Update this daily
   theme: "Dates that changed the course of human history",
   numbers: ['07', '20', '69', '12', '25', '00', '11', '09', '89', '09', '11', '01'],
   correctDates: [
-    { date: '07/20/69', event: "Apollo 11 Moon Landing: Humanity's first steps on another celestial body." },
-    { date: '12/25/00', event: "First crew arrives at the International Space Station, marking the beginning of continuous human presence in space." },
-    { date: '11/09/89', event: "Fall of the Berlin Wall, symbolizing the end of the Cold War and German reunification." },
-    { date: '09/11/01', event: "September 11 attacks in the United States, reshaping global politics and security." }
+    { 
+      date: '07/20/69', 
+      event: "Apollo 11 Moon Landing: Humanity's first steps on another celestial body.", 
+      link: "https://www.nasa.gov/mission_pages/apollo/apollo11.html",
+      photo: "https://th.bing.com/th?id=OSK.8A70FA074852897CF8F8F60292DB3A9E053432A7512B8426D15C7E13AEF2EC7B&w=127&h=142&o=6&dpr=1.8&pid=SANGAM"
+    },
+    { 
+      date: '12/25/00', 
+      event: "First crew arrives at the International Space Station, marking the beginning of continuous human presence in space.", 
+      link: "https://www.nasa.gov/mission_pages/station/main/index.html",
+      photo: "https://th.bing.com/th/id/OIP.8k0lpVClxk2FZjXbAwrQNQHaGU?pid=ImgDet&w=205&h=174&c=7&dpr=1.8"
+    },
+    { 
+      date: '11/09/89', 
+      event: "Fall of the Berlin Wall, symbolizing the end of the Cold War and German reunification.", 
+      link: "https://www.history.com/topics/cold-war/fall-of-berlin-wall",
+      photo: "https://th.bing.com/th/id/OIP.YHSNtadztGK_NGsrrwkVewHaE8?w=260&h=180&c=7&r=0&o=5&dpr=1.8&pid=1.7"
+    },
+    { 
+      date: '09/11/01', 
+      event: "September 11 attacks in the United States, reshaping global politics and security.", 
+      link: "https://www.911memorial.org/",
+      photo: "https://th.bing.com/th/id/OIP.1OKExd5nB_52KckhYBmWQwHaHa?w=176&h=180&c=7&r=0&o=5&dpr=1.8&pid=1.7"
+    }
   ]
 };
 
