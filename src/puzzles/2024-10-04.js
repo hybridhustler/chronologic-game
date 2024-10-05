7436bb5100ce90b904433ccf5859a3dea51da371
hybridhustler <hybridhustlerarmy@gmail.com>
2024-10-05 09:56:08 -0400

    new puzzle

---
+++ src/puzzles/2024-10-04.js
@@ -0,0 +1,34 @@
+const dailyPuzzle = {
+  date: '2024-10-04',
+  gameNumber: 2,
+  theme: "Tune in! Famous TV show finales",
+  numbers: ['05', '06', '04', '05', '23', '10', '05', '22', '18', '04', '07', '20'],
+  correctDates: [
+    { 
+      date: '05/06/04', 
+      event: "Final episode of 'Friends': After 10 seasons, the beloved show about six friends in New York City concludes.", 
+      link: "https://www.imdb.com/title/tt0108778/",
+      photo: "https://th.bing.com/th/id/OIP.L9oFwswA4avGdx7ghfepoAHaF7?w=223&h=180&c=7&r=0&o=5&dpr=1.8&pid=1.7"
+    },
+    { 
+      date: '05/23/10', 
+      event: "Series finale of 'Lost': The mysterious island show's final episode airs, leaving viewers with lingering questions.", 
+      link: "https://www.imdb.com/title/tt0411008/",
+      photo: "https://th.bing.com/th/id/OIP.E5JYGhxFsdF9S7uBszZh0AHaEK?w=302&h=180&c=7&r=0&o=5&dpr=1.8&pid=1.7"
+    },
+    { 
+      date: '05/22/18', 
+      event: "Last episode of 'The Middle': The quirky Heck family says goodbye after 9 seasons of life in the Midwest.", 
+      link: "https://www.imdb.com/title/tt1442464/",
+      photo: "https://th.bing.com/th/id/OIP.uKn4PjXEFcAHLRrC1m76TQHaF5?w=249&h=202&c=7&r=0&o=5&dpr=1.8&pid=1.7"
+    },
+    { 
+      date: '04/07/20', 
+      event: "Series finale of 'Schitt's Creek': The Canadian sitcom about the Rose family's misadventures comes to a heartwarming end.", 
+      link: "https://www.imdb.com/title/tt3526078/",
+      photo: "https://th.bing.com/th/id/OIP.A5fhkhIRUCBg3-2HFSB9jAHaEK?w=302&h=180&c=7&r=0&o=5&dpr=1.8&pid=1.7"
+    }
+  ]
+};
+
+export default dailyPuzzle;
