140a2c10e754602d36a78ff9e04fd112cf9468df
hybridhustler <hybridhustlerarmy@gmail.com>
2024-10-06 20:31:12 -0400

    daily puzzle

---
+++ src/puzzles/2024-10-07.js
@@ -0,0 +1,34 @@
+const dailyPuzzle = {
+    date: '2024-10-07',
+    gameNumber: 5,
+    theme: "Groundbreaking Scientific Discoveries",
+    numbers: ['09', '28', '28', '11', '25', '54', '07', '16', '12', '07', '04', '11'],
+    correctDates: [
+      { 
+        date: '09/28/28', 
+        event: "Discovery of penicillin: Alexander Fleming discovers the world's first antibiotic, revolutionizing medicine.", 
+        link: "https://www.nobelprize.org/prizes/medicine/1945/fleming/biographical/",
+        hint: "The accidental mold that saved millions"
+      },
+      { 
+        date: '11/25/54', 
+        event: "The C60 molecule (Buckminsterfullerene) is first discovered: Leading to new fields of nanotechnology and materials science.", 
+        link: "https://www.nobelprize.org/prizes/chemistry/1996/",
+        hint: "The soccer-ball-shaped molecule"
+      },
+      { 
+        date: '07/16/12', 
+        event: "Discovery of the Higgs boson: The particle that gives mass to other particles, confirmed at CERN.", 
+        link: "https://home.cern/science/physics/higgs-boson",
+        hint: "The 'God particle' confirmed at CERN"
+      },
+      { 
+        date: '07/04/11', 
+        event: "First detection of gravitational waves: Proving Einstein's theory of relativity in a groundbreaking astrophysical observation.", 
+        link: "https://www.nobelprize.org/prizes/physics/2017/",
+        hint: "Ripples in spacetime"
+      }
+    ]
+  };
+
+export default dailyPuzzle;
---
+++ src/puzzles/2024-10-08.js
@@ -0,0 +1,34 @@
+const dailyPuzzle = {
+    date: '2024-10-08',
+    gameNumber: 6,
+    theme: "Famous Firsts in Technology",
+    numbers: ['10', '29', '69', '12', '03', '82', '08', '06', '05', '04', '89', '28'],
+    correctDates: [
+      { 
+        date: '10/29/69', 
+        event: "The first ARPANET message: The precursor to the internet is sent from UCLA to Stanford.", 
+        link: "https://www.livinginternet.com/i/ii_arpanet.htm",
+        hint: "The message was supposed to be 'login', but it crashed after two letters."
+      },
+      { 
+        date: '12/03/82', 
+        event: "First public mobile phone call: Made by Martin Cooper of Motorola, marking the dawn of mobile communication.", 
+        link: "https://www.telegraph.co.uk/technology/news/10766878/Worlds-first-mobile-phone-call-40-years-on.html",
+        hint: "The birth of handheld communication"
+      },
+      { 
+        date: '08/06/91', 
+        event: "The World Wide Web goes public: Tim Berners-Lee posts the first public webpage, changing the way we access information.", 
+        link: "https://home.cern/science/computing/birth-web",
+        hint: "You’re browsing the internet thanks to this moment"
+      },
+      { 
+        date: '05/04/89', 
+        event: "Launch of the Game Boy: Nintendo's iconic handheld gaming device is released, revolutionizing portable gaming.", 
+        link: "https://www.nintendo.co.jp/clv/manuals/en/pdf/CLV-P-HAAJE.pdf",
+        hint: "Portable gaming in your pocket"
+      }
+    ]
+  };
+
+export default dailyPuzzle;
